296b0223f4cb136da964e2820c1f9a7f51ef90a8
luhoro <luisashorowitz@gmail.com>
2024-01-04 22:28:20 -0300

    Test(initial page): First basic test

---
+++ cypress/e2e/inicio.cy.js
@@ -0,0 +1,6 @@
+describe('template spec', () => {
+  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2').contains('Vantagens do nosso banco:');
+  });
+});
