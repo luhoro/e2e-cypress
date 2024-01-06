672b1fb093e43aac125382742b555098ecb8daa4
luhoro <luisashorowitz@gmail.com>
2024-01-06 18:27:54 -0300

    Test: New custom command

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,6 +1,11 @@
-describe('template spec', () => {
-  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('h2').contains('Vantagens do nosso banco:');
-  });
-});
+describe('Página inicial', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal')
+      .contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+      )
+  })
+})
