6c658268cbe5f15340024bbe807bd528f0444f2f
luhoro <luisashorowitz@gmail.com>
2024-01-25 22:28:29 -0300

    Build: Update base project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,3 @@
 Cypress.Commands.add('getByData', (seletor) => {
   return cy.get(`[data-test=${seletor}]`)
 })
-
-Cypress.Commands.add('verifyText', (seletor, text) => {
-  cy.get(`${seletor}`).contains(`${text}`)
-})
