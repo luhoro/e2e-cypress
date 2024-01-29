13e2a4f6c16e8ba738d7a2c7214b9847aa2f8fb9
luhoro <luisashorowitz@gmail.com>
2024-01-28 22:24:20 -0300

    Test: Using session validation

--- cypress/e2e/formularioLogin.cy.js
+++ cypress/e2e/formularioLogin.cy.js
@@ -1,11 +1,14 @@
 describe('Formulário de login', () => {
-  beforeEach(() => {
-    cy.visit('/')
+
+  it.only('Deve acessar a página home', () => {
+    cy.login('luisa@gmail.com', 'Aa')
+    cy.visit('/home')
+    cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta')
   })
 
   it('Não deve permitir um e-mail inválido', () => {
     cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('luisa@gmail')
+    cy.getByData('email-input').type('luisa@gmail.com')
     cy.getByData('senha-input').type('Aa')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,18 @@
 Cypress.Commands.add('getByData', (seletor) => {
   return cy.get(`[data-test=${seletor}]`)
 })
+
+Cypress.Commands.add('verifyText', (seletor, text) => {
+  cy.get(`${seletor}`).contains(`${text}`)
+})
+
+Cypress.Commands.add('login', (email, senha) => {
+  cy.session([email, senha], () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type(email)
+    cy.getByData('senha-input').type(senha)
+    cy.getByData('botao-enviar').click()
+    cy.url().should('contains', '/home')
+  })
+})
